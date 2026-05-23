55c74621ace2ea8fd85d3874baf2e12bc7f4bb48
NITHIN REDDY BOMMIREDDY <nithinreddybommireddy@gmail.com>
2026-05-23 11:05:50 +0530

    ✅ Add solution for: Active Customers with Default Shipping Address - SQL

---
+++ exercises/ActiveCustomersWithDefaultShippingAddressSQL.sql
@@ -0,0 +1,10 @@
+-- Write a SQL query below --
+SELECT 
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    c.email,
+    a.city
+FROM customers c
+INNER JOIN addresses a
+    ON c.id = a.customer_id WHERE c.is_active = true
+  AND a.is_default = true
+ORDER BY c.first_name ASC;
