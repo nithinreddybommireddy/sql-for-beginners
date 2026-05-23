73dadf4c910f25186483f45fbcbe823547608d64
NITHIN REDDY BOMMIREDDY <nithinreddybommireddy@gmail.com>
2026-05-23 11:40:30 +0530

    ✅ Add solution for: Orders and Payment Status Tracking - SQL

---
+++ exercises/OrdersAndPaymentStatusTrackingSQL.sql
@@ -0,0 +1,9 @@
+-- Write a SQL query below --
+SELECT o.id AS order_id,c.email,ROUND(o.total_amount,2) AS total_amount,p.payment_method
+FROM orders o
+ JOIN customers c ON o.customer_id = c.id
+JOIN payments p ON o.id = p.order_id
+WHERE o.ordered_at >= '2023-10-01'
+  AND o.ordered_at < '2023-11-01'
+ORDER BY o.id ASC;
+
